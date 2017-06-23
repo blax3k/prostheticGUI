696ae6b7ecdcdd5a855a6717d0175b26eb5d5bf9
blax3k <blax3k@gmail.com>
2017-06-23 14:57:09 -0700

    basic functionality up

---
+++ testGUI/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace testGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonLED = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonSyncTime = new System.Windows.Forms.Button();
+            this.labelSerial = new System.Windows.Forms.Label();
+            this.labelFirmware = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLED
+            // 
+            this.buttonLED.Location = new System.Drawing.Point(43, 213);
+            this.buttonLED.Name = "buttonLED";
+            this.buttonLED.Size = new System.Drawing.Size(75, 23);
+            this.buttonLED.TabIndex = 0;
+            this.buttonLED.Text = "button1";
+            this.buttonLED.UseVisualStyleBackColor = true;
+            this.buttonLED.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 195);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonSyncTime
+            // 
+            this.buttonSyncTime.Location = new System.Drawing.Point(165, 213);
+            this.buttonSyncTime.Name = "buttonSyncTime";
+            this.buttonSyncTime.Size = new System.Drawing.Size(75, 23);
+            this.buttonSyncTime.TabIndex = 2;
+            this.buttonSyncTime.Text = "Sync Time";
+            this.buttonSyncTime.UseVisualStyleBackColor = true;
+            this.buttonSyncTime.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelSerial
+            // 
+            this.labelSerial.AutoSize = true;
+            this.labelSerial.Location = new System.Drawing.Point(13, 519);
+            this.labelSerial.Name = "labelSerial";
+            this.labelSerial.Size = new System.Drawing.Size(37, 13);
+            this.labelSerial.TabIndex = 3;
+            this.labelSerial.Text = "serial: ";
+            // 
+            // labelFirmware
+            // 
+            this.labelFirmware.AutoSize = true;
+            this.labelFirmware.Location = new System.Drawing.Point(450, 519);
+            this.labelFirmware.Name = "labelFirmware";
+            this.labelFirmware.Size = new System.Drawing.Size(52, 13);
+            this.labelFirmware.TabIndex = 4;
+            this.labelFirmware.Text = "firmware: ";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(374, 77);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(36, 13);
+            this.labelTime.TabIndex = 5;
+            this.labelTime.Text = "Time: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 544);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelFirmware);
+            this.Controls.Add(this.labelSerial);
+            this.Controls.Add(this.buttonSyncTime);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonLED);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Device Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLED;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonSyncTime;
+        private System.Windows.Forms.Label labelSerial;
+        private System.Windows.Forms.Label labelFirmware;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ testGUI/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testGUI
+{
+    public partial class Form1 : Form
+    {
+        public delegate void SetTextDelegate(String myString);
+        public SetTextDelegate textDelegate; //an instance of AddDataDelegate
+        public delegate void AddDataDelegate_button(String myString);
+        public AddDataDelegate_button myDelegate_button;
+        public delegate void setSerialDelegate(String serial);
+        public setSerialDelegate serialDelegate;
+        public delegate void setFirmwrDelegate(String myString);
+        public setFirmwrDelegate firmwrDelegate;
+        public delegate void setTimeDelegate(String myString);
+        public setTimeDelegate timeDelegate;
+        bool status = false;
+        volatile bool running = false;
+        string firmware = "";
+        string serial = "";
+        const string STATE = "STATE=",
+            INFO = "INFO=",
+            GETTIME = "GETTIME=",
+            SYNCTIME = "SYNCTIME";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        peforms initial setup and makes sure that there is a valid device connected
+        */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            running = true;
+            this.textDelegate = new SetTextDelegate(AddDataMethod);
+            this.myDelegate_button = new AddDataDelegate_button(setButtonLEDText);
+            this.serialDelegate = new setSerialDelegate(setSerial);
+            this.firmwrDelegate = new setFirmwrDelegate(setFirmware);
+            this.timeDelegate = new setTimeDelegate(setTime);
+
+            try
+            {                       //try to establish a connection
+                serialPort1.Open();
+                serialPort1.WriteLine(STATE);
+                //setup Serial number
+                serialPort1.WriteLine(INFO);
+                serialPort1.WriteLine(GETTIME);
+
+                Thread timeThread = new Thread(new ThreadStart(this.startTimeThread));
+                timeThread.Start();
+
+            }
+            catch (Exception exception) //no device detected
+            {
+                MessageBox.Show("No device detected" + "\n" + exception, "Load Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void startTimeThread()
+        {
+            while (running)
+            {
+                serialPort1.WriteLine(GETTIME);
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+
+        /*
+
+        */
+        private void setButtonLEDText(string myString)
+        {
+            buttonLED.Text = myString;
+        }
+        /*
+        set the text in the rich text box
+        */
+        private void AddDataMethod(string myString)
+        {
+            richTextBox1.Text = richTextBox1.Text + myString + Environment.NewLine;
+        }
+        /*
+        sets the serial number label
+        */
+        private void setSerial(string serial)
+        {
+            labelSerial.Text = "serial: " + serial;
+        }
+        /*
+        sets the firmware label
+        */
+        private void setFirmware(string firmware)
+        {
+            labelFirmware.Text = "firmware: " + firmware;
+        }
+        /*
+        sets the time of the time label
+        */
+        private void setTime(string time)
+        {
+            labelTime.Text = time;
+        }
+        /*
+        handles input from the serial port
+        */
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string s = serialPort1.ReadLine(); //get the data
+            if (s.Contains("STATE=")) //received the LED state
+            {
+                s = s.Trim();
+                string new_s = s;
+                Console.WriteLine("read State");
+                //string new_s = s.Replace("state=", "");
+                if (new_s.Contains("0"))
+                {
+                    status = false;
+                    buttonLED.Invoke(this.myDelegate_button, new Object[] { "ON" });
+                }
+                else
+                {
+                    status = true;
+                    buttonLED.Invoke(this.myDelegate_button, new Object[] { "OFF" });
+                }
+            }
+            else if (s.Contains("info=")) //starts as "info=firmware~serial"
+            {
+                s = s.Trim();
+                s = s.Replace("info=", ""); //becomes "firmware~serial"
+                string[] infoArray = s.Split('~'); //split into firmware and serial
+                labelSerial.Invoke(this.serialDelegate, new Object[] { infoArray[1] });
+                labelFirmware.Invoke(this.firmwrDelegate, new Object[] { infoArray[0] });
+            }
+            else if (s.Contains("time="))
+            {
+                //s.Replace("time=", "");
+                s = s.Trim();
+                s = s.Replace("time=", "Time: ");
+                labelTime.Invoke(this.timeDelegate, new Object[] { s });
+            }
+            else
+            {
+                richTextBox1.Invoke(this.textDelegate, new Object[] { s });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (buttonLED.Text.Contains("ON"))
+            {
+                serialPort1.WriteLine("OFF");
+                buttonLED.Text = "OFF";
+                status = false;
+            }
+            else
+            {
+                serialPort1.WriteLine("ON");
+                buttonLED.Text = "ON";
+                status = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+        }
+
+        /*
+        Sync Time button
+        */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int32 unixTimestamp = (Int32)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            string unixDate = SYNCTIME;
+            unixDate += unixTimestamp + '=';
+            serialPort1.WriteLine(unixDate);
+            //serialPort1.WriteLine("T1262347200");
+        }
+
+        public class Interfacex
+        {
+
+        }
+    }
+}
